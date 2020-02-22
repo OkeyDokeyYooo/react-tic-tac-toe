128d5945df2720238069e8d93dc30c8d3c540e42
OkeyDokeyYooo <873262667a@gmail.com>
2020-02-21 22:41:42 -0800

    first commit

---
+++ src/component/Board.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../App.css'
+
+class Board extends Component {
+
+    render() {
+        return (
+            <div className="board">
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+            </div>
+        )
+    }
+}
+
+export default Board;
